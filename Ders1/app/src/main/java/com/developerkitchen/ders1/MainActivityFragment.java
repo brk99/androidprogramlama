ca6fc8ed4e08bc0300dce97900c084dc89f14423
İbrahim Gündüz <igunduz91@gmail.com>
2016-05-17 01:38:49 +0300

    video2 ders2 kodlarını ekledim.

--- Ders1/app/src/main/java/com/developerkitchen/ders1/MainActivityFragment.java
+++ Ders1/app/src/main/java/com/developerkitchen/ders1/MainActivityFragment.java
@@ -5,17 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private TextView textView1;
+    private Button button;
 
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        textView1 = (TextView) rootView.findViewById(R.id.textView1);
+        textView1.setText("Benim adım ibrahim gündüz.");
+
+        button = (Button) rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //yapılan işlem
+                textView1.setText(textView1.getText().toString() + "/n" + "Ben android programcısıyım.");
+            }
+        });
+
+        return rootView;
     }
 }
